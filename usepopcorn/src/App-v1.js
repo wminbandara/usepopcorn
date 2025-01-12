29d74f0bb849ff58622a6e8dad431236b9961d53
wminbandara <wminbandara@gmail.com>
2025-01-12 09:21:25 +0530

    How NOT to Fetch Data in React

--- usepopcorn/src/App-v1.js
+++ usepopcorn/src/App-v1.js
@@ -50,16 +50,9 @@ const tempWatchedData = [
 const average = (arr) =>
   arr.reduce((acc, cur, i, arr) => acc + cur / arr.length, 0);
 
-const KEY = "2a86732c";
-
 export default function App() {
-  const [movies, setMovies] = useState([]);
-  const [watched, setWatched] = useState([]);
-
-  fetch(`http://www.omdbapi.com/?apikey=${KEY}&s=matrix`)
-    .then((res) => res.json())
-    .then((data) => console.log(data));
-  //.then((data) => setMovies(data.Search));
+  const [movies, setMovies] = useState(tempMovieData);
+  const [watched, setWatched] = useState(tempWatchedData);
 
   return (
     <>
--- usepopcorn/src/index.js
+++ usepopcorn/src/index.js
@@ -1,30 +1,18 @@
-import React, { useState } from "react";
+import React from "react";
 import ReactDOM from "react-dom/client";
-// import "./index.css";
-// import App from "./App";
-import StarRating from "./StarRating";
-
-function Test() {
-  const [movieRating, setMovieRating] = useState(0);
-
-  return (
-    <div>
-      <StarRating colour="blue" maxRating={10} onSetRating={setMovieRating} />
-      <p>This movie was rated {movieRating} Stars</p>
-    </div>
-  );
-}
+import "./index.css";
+import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    {/*<App />*/}
-    <StarRating
+    <App />
+    {/* <StarRating
       maxRating={5}
       messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]}
     />
     <StarRating size={24} color="red" className="test" defaultRating={2} />
 
-    <Test />
+    <Test /> */}
   </React.StrictMode>
 );
