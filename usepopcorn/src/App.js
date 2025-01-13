85888d6fc0d8c4627166caa1bbff20ccf93055ec
wminbandara <wminbandara@gmail.com>
2025-01-13 23:34:52 +0530

    One More Effect Listening to a Keypress

--- usepopcorn/src/App.js
+++ usepopcorn/src/App.js
@@ -365,6 +365,22 @@ function MovieDetails({ selectedId, onCloseMovie, onAddWatched, watched }) {
     onCloseMovie();
   }
 
+  //Key Press Event
+  useEffect(
+    function () {
+      function callback(e) {
+        if (e.code === "Escape") onCloseMovie();
+      }
+
+      document.addEventListener("keydown", callback);
+
+      return function () {
+        document.removeEventListener("keydown", callback);
+      };
+    },
+    [onCloseMovie]
+  );
+
   useEffect(
     function () {
       async function getMovieDetails() {
