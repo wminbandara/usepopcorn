2073355dae1f3e2f26ef56d0d588bddad0c1cfa7
wminbandara <wminbandara@gmail.com>
2025-01-14 20:35:51 +0530

    Count howmany times user change his star rating using Refs to Persist Data Between Renders

--- usepopcorn/src/App.js
+++ usepopcorn/src/App.js
@@ -292,6 +292,15 @@ function MovieDetails({ selectedId, onCloseMovie, onAddWatched, watched }) {
   const [isLoading, setIsLoading] = useState(false);
   const [userRating, setUserRating] = useState("");
 
+  const countRef = useRef(0);
+
+  useEffect(
+    function () {
+      if (userRating) countRef.current++;
+    },
+    [userRating]
+  );
+
   const isWatched = watched.map((movie) => movie.imdbID).includes(selectedId);
 
   const watchedUserRating = watched.find(
@@ -320,6 +329,7 @@ function MovieDetails({ selectedId, onCloseMovie, onAddWatched, watched }) {
       imdbRating: Number(imdbRating),
       runtime: Number(runtime.split(" ")[0]),
       userRating,
+      countRatingDecisions: countRef.current,
     };
 
     onAddWatched(newWatchedMovie);
